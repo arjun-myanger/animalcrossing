e91006f4862f4178fe3b970fbac1625ae22229d5
arjun-myanger <admyanger@outlook.com>
2023-05-16 16:18:48 +0100

    styling changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 // components/Header.tsx
 import React, { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -18,12 +17,12 @@ const Header: React.FC = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-black bg-opacity-50 shadow-md">
       <div className="container mx-auto py-4 flex justify-between items-center">
         <Link href="/">
           <div className="flex items-center cursor-pointer">
             <Image src="/leaf.svg.png" alt="Leaf Logo" width={40} height={40} />
-            <span className="ml-2 text-3xl font-semibold text-darker-green hover:text-darker-blue transition-colors duration-300 link-underline-animation">
+            <span className="ml-2 text-3xl font-semibold text-white hover:text-green-500 transition-colors duration-300">
               Animal Crossing
             </span>
           </div>
@@ -31,7 +30,7 @@ const Header: React.FC = () => {
         <nav className="hidden md:flex space-x-6">
           {games.map((game, index) => (
             <Link key={index} href={game.path}>
-              <span className="text-lg font-medium cursor-pointer text-darker-green hover:text-darker-blue transition-colors duration-300 link-underline-animation">
+              <span className="text-lg font-medium cursor-pointer text-white hover:text-green-500 transition-colors duration-300">
                 {game.title}
               </span>
             </Link>
@@ -41,48 +40,20 @@ const Header: React.FC = () => {
           className="md:hidden focus:outline-none"
           onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         >
-          <FontAwesomeIcon
-            icon={faBars}
-            className="w-8 h-8 text-darker-green"
-          />
+          <FontAwesomeIcon icon={faBars} className="w-8 h-8 text-white" />
         </button>
       </div>
       {mobileMenuOpen && (
         <nav className="flex flex-col space-y-2 md:hidden mt-4">
           {games.map((game, index) => (
             <Link key={index} href={game.path}>
-              <span className="text-lg font-medium cursor-pointer text-darker-green hover:text-darker-blue transition-colors duration-300 link-underline-animation">
+              <span className="text-lg font-medium cursor-pointer text-white hover:text-green-500 transition-colors duration-300">
                 {game.title}
               </span>
             </Link>
           ))}
         </nav>
       )}
-      <style jsx>{`
-        .text-darker-green {
-          color: #72b381;
-        }
-        .hover:text-darker-blue:hover {
-          color: #51a5 5c0;
-        }
-        .link-underline-animation {
-          position: relative;
-        }
-        .link-underline-animation::before {
-          content: "";
-          position: absolute;
-          bottom: -2px;
-          left: 0;
-          width: 100%;
-          height: 2px;
-          background-color: currentColor;
-          transform: scaleX(0);
-          transition: transform 0.3s ease-in-out;
-        }
-        .link-underline-animation:hover::before {
-          transform: scaleX(1);
-        }
-      `}</style>
     </header>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,12 +7,12 @@ const Hero: React.FC = () => {
       className="py-16 px-4 sm:px-8 lg:px-16 min-h-screen"
       style={{
         backgroundImage: "url(/leaves.png)",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
+        backgroundSize: "200px 200px",
+        backgroundRepeat: "repeat",
       }}
     >
       <div className="container mx-auto max-w-screen-lg flex flex-col h-full">
-        <div className="text-center bg-black bg-opacity-50 text-white p-6 mb-8">
+        <div className="text-center bg-black bg-opacity-50 text-white p-6 mb-8 rounded-lg">
           <h1 className="text-4xl md:text-5xl font-semibold mb-6">
             Welcome to Animal Crossing Central
           </h1>
@@ -23,7 +23,7 @@ const Hero: React.FC = () => {
           </p>
         </div>
         <div
-          className="w-full bg-cover bg-center mb-8"
+          className="w-full bg-cover bg-center mb-8 rounded-lg"
           style={{ backgroundImage: "url(/acnh.jpg)", height: "400px" }}
         />
       </div>
